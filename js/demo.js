3d42f4b70259f54040f4ff97bd21736c9a9ff85b
Janos Rusiczki <janos.rusiczki@gmail.com>
2012-03-06 22:26:31 +0200

    Some more demonstrative material.

--- js/demo.js
+++ js/demo.js
@@ -1,5 +1,19 @@
-var Movie = Backbone.Model.extend();
+var Movie = Backbone.Model.extend({
+	validate: function (attrs) {
+		if (!_.isString(attrs.title) || attrs.title.length === 0 ) {
+			return "Don't be silly, a movie without a title!?";
+		}
+	}
+});
 
 var movie = new Movie();
 
-movie.set({ title: "Test" });
+movie.on("error", function(model, error) {
+  console.log(error);
+});
+
+movie.set({ title: 12 });
+
+movie.set({ title: "The Big Lebowski" })
+
+console.log(movie.get("title"));
