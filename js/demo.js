26e11cdbabb8ea48fb5c1dfdc50796c44f21caaa
Janos Rusiczki <janos.rusiczki@gmail.com>
2012-03-07 14:56:50 +0200

    Working Backbone model, collection and view.

--- js/demo.js
+++ js/demo.js
@@ -6,14 +6,63 @@ var Movie = Backbone.Model.extend({
 	}
 });
 
+var Movies = Backbone.Collection.extend({
+	model: Movie,
+	url: 'api/movies',
+	display: function() {
+		this.forEach(function(el) { console.log( el.get('title') ) });
+	},
+});
+
 var movie = new Movie();
 
 movie.on("error", function(model, error) {
   console.log(error);
 });
 
-movie.set({ title: 12 });
+var movies = new Movies();
+// movies.fetch();
 
-movie.set({ title: "The Big Lebowski" })
+movie.set({ title: "Snatch!" });
+movies.create({ title: "a" });
 
-console.log(movie.get("title"));
+// movies.display();
+
+var MovieView = Backbone.View.extend({
+		tagName: "li",
+		template: _.template($('#item-template').html()),
+		initialize: function() {
+			/*
+			this.model.bind('change', this.render, this);
+			this.model.bind('destroy', this.render, this);
+			*/
+		},
+		render: function() {
+			$(this.el).html(this.template(this.model.toJSON()));
+      // (this.model.get('title'));
+      return this;
+		}
+});
+
+var AppView = Backbone.View.extend({
+		el: $('#app'),
+		initialize: function() {
+			movies.bind('add', this.addOne, this);		
+			movies.bind('reset', this.addAll, this);
+			movies.bind('all', this.render, this);
+			
+			movies.fetch();
+		},
+		addOne: function(movie) {
+			var movieView = new MovieView({model: movie});
+			$("#movie-list").append(movieView.render().el);
+		},
+		addAll: function() {
+			movies.each(this.addOne);
+		},
+		render: function() {
+			console.log('Rendering AppView.');
+		},
+});
+
+var app = new AppView();
