94f4c69170681d2fe0d22321eb71a7724ac410e3
Janos Rusiczki <janos.rusiczki@gmail.com>
2012-03-08 20:29:14 +0200

    It looks like something was really necessary! :)

--- js/demo.js
+++ js/demo.js
@@ -7,69 +7,71 @@ var Movies = Backbone.Collection.extend({
 var movies = new Movies();
 
 var MovieView = Backbone.View.extend({
-		tagName: "li",
-		template: _.template($('#item-template').html()),
-		
-		initialize: function() {		
-			this.model.bind('destroy', this.remove, this);
-		},
-		
-		render: function() {
-			console.log('Rendering MovieView.');
-			$(this.el).html(this.template(this.model.toJSON()));
-			return this;
-		},
-		
-		events: {
-			"click": "deleteMovie",
-		},
-		
-		deleteMovie: function() {
-			if(confirm('Are you sure that you want to delete ' + this.model.get('title') + '?')) {
-				console.log('Destroying Movie: id = ' + this.model.id);
-				this.model.destroy();
-			}
-		},
-		
-		remove: function() {
-			$(this.el).remove();
+	tagName: "li",
+	template: _.template($('#item-template').html()),
+	
+	initialize: function() {		
+		this.model.bind('destroy', this.remove, this);
+	},
+	
+	render: function() {
+		console.log('Rendering MovieView.');
+		$(this.el).html(this.template(this.model.toJSON()));
+		return this;
+	},
+	
+	events: {
+		"click": "deleteMovie",
+	},
+	
+	deleteMovie: function() {
+		if(confirm('Are you sure that you want to delete ' + this.model.get('title') + '?')) {
+			console.log('Destroying Movie: id = ' + this.model.id);
+			this.model.destroy();
 		}
+	},
+	
+	remove: function() {
+		$(this.el).remove();
+	}
 });
 
 var AppView = Backbone.View.extend({
-		initialize: function() {
-			console.log('Initializing AppView.');
-			
-			movies.bind('add', this.addOne, this); // I mean this binding	
-			movies.bind('reset', this.addAll, this);
+	el: $('#app'),
+	
+	initialize: function() {
+		console.log('Initializing AppView.');
+	
+		movies.bind('add', this.addOne, this); // I mean this binding	
+		movies.bind('reset', this.addAll, this);
 
-			movies.fetch(); // Will call Movies.fetch() -> Movies.reset() -> (via binding above) -> AppView.addAll()
-		},
-		
-		addAll: function() {
-			console.log('Adding all MovieViews.');
-			movies.each(this.addOne);
-		},
-		
-		addOne: function(movie) {
-			console.log('Adding a MovieView.');
-			var movieView = new MovieView({model: movie}); // Will create a MovieView
-			$("#movie-list").append(movieView.render().el); // Will render and add the movieView
-		},
-		
-		render: function() {
-			console.log('Rendering AppView.');
-		},
-		
-		events: {
-			"click #add-movie": "addMovie",
-		},
-		
-		addMovie: function() {
-			movies.create({"title": $('#movie-title').val(), "year": $('#movie-year').val()});
-			$('#movie-title').val('');
-			$('#movie-year').val('');
-		},
+		movies.fetch(); // Will call Movies.fetch() -> Movies.reset() -> (via binding above) -> AppView.addAll()
+	},
+
+	addAll: function() {
+		console.log('Adding all MovieViews.');
+		movies.each(this.addOne);
+	},
+
+	addOne: function(movie) {
+		console.log('Adding a MovieView.');
+		var movieView = new MovieView({model: movie}); // Will create a MovieView
+		$("#movie-list").append(movieView.render().el); // Will render and add the movieView
+	},
+
+	render: function() {
+		console.log('Rendering AppView.');
+	},
+
+	events: {
+		"click #add-movie": "addMovie",
+	},
+
+	addMovie: function() {
+		movies.create({"title": $('#movie-title').val(), "year": $('#movie-year').val()});
+		$('#movie-title').val('');
+		$('#movie-year').val('');
+	},
 });
 
 var app = new AppView(); // Will call AppView.initialize
